9a1323429e300d24889b859ee7e1f7f0cb1d3081
Matthew Froggatt <froggatt.me@gmail.com>
2016-03-08 07:48:40 -0500

    Add front-end form to submit message

--- public/js/app.js
+++ public/js/app.js
@@ -8,3 +8,18 @@ socket.on('message', function(message) {
   console.log('New message: ');
   console.log(message.text);
 });
+
+// Handles submitting of new message
+var $form = $('#message-form');
+
+$form.on('submit', function(event) {
+  event.preventDefault();
+
+  var $message = $form.find('input[name=message]');
+
+  socket.emit('message', {
+    text: $message.val()
+  });
+  $message.val('');
+
+});
