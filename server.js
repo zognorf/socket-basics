81f61ffb7f200ae5d0f048cc38710f6d8ac1c672
Matthew Froggatt <froggatt.me@gmail.com>
2016-03-18 12:17:00 -0400

    Add bootstrap and style join page

--- server.js
+++ server.js
@@ -9,6 +9,30 @@ app.use(express.static(__dirname + '/public'));
 
 var clientInfo = {};
 
+// Sends current users to provided socket
+function sendCurrentUsers(socket) {
+  var info = clientInfo[socket.id];
+  var users = [];
+
+  if (typeof info === 'undefined') {
+    return;
+  }
+
+  Object.keys(clientInfo).forEach(function(socketId) {
+    var userInfo = clientInfo[socketId];
+
+    if (info.room === userInfo.room) {
+      users.push(userInfo.name);
+    }
+  });
+
+  socket.emit('message', {
+    name: 'System',
+    text: 'Current users: ' + users.join(', '),
+    timestamp: moment.valueOf
+  });
+}
+
 io.on('connection', function(socket) {
   console.log('User connected via socket.io!');
 
@@ -38,8 +62,12 @@ io.on('connection', function(socket) {
   socket.on('message', function(message) {
     console.log('Message received: ' + message.text);
 
-    message.timestamp = moment().valueOf();
-    io.to(clientInfo[socket.id].room).emit('message', message);
+    if (message.text === '@currentUsers') {
+      sendCurrentUsers(socket);
+    } else {
+      message.timestamp = moment().valueOf();
+      io.to(clientInfo[socket.id].room).emit('message', message);
+    }
   });
 
   socket.emit('message', {
